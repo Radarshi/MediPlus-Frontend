971b269bd65106d5cabba931ab0fabd606b23985
Radarshi <radarshim2@gmail.com>
2026-02-04 18:17:31 +0530

    Signup Path fix

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import AuthForm from '../components/AuthForm';
+import AuthForm from '../components/Authform.jsx';
 import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
